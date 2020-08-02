d22046a46b1ba0cc180d04aa9e44f659dc5d42d7
shin donggyu <donggyu9410@gmail.com>
2020-08-02 19:29:33 +0900

    get current geolocation

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import TabNavigator from "./navigations/TabNavigator";
+import { Alert } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  useEffect(() => {
+    findCoordinates();
+  });
+
+  const findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const location = JSON.stringify(position);
+
+        console.log(location);
+      },
+      (error) => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+
   return (
     <NavigationContainer>
       <TabNavigator />
